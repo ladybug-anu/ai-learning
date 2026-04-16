dd2cf466ab9a38c0f7e15b88eb138f1c99a0c6ed
Anu Priya <39757552+ladybug-anu@users.noreply.github.com>
2026-04-16 11:14:34 +0530

    streaming output and streaming chatbot day 4

--- week-01/streaming.py
+++ week-01/streaming.py
@@ -24,6 +24,6 @@ stream = client.chat.completions.create(
 for chunk in stream:
     content = chunk.choices[0].delta.content
     if content is not None:
-        print(content , end = "")
+        print(content , end = "", flush = True)
 
-print('\n')
+print('\n ')
--- week-01/streaming_chatbot.py
+++ week-01/streaming_chatbot.py
@@ -1,6 +1,6 @@
 from groq import Groq
 from dotenv import load_dotenv
-import os
+import os 
 
 load_dotenv()
 
