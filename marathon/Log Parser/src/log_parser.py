812cc04c27b45509fd6024ec1e91eebdd3a7d579
Anu Priya <39757552+ladybug-anu@users.noreply.github.com>
2026-08-16 14:26:39 +0530

    skeleton of log parser

---
+++ marathon/Log Parser/src/log_parser.py
@@ -0,0 +1,18 @@
+def read_log():
+    pass
+
+def parse_line():
+    pass
+
+def total_lines():
+    pass
+
+def count_per_level():
+    pass
+
+def common_messages():
+    pass
+
+def save_report_to_json():
+    pass
+
