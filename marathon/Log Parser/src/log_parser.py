45d8a8081c6b04d6d09a7d3c6b5c70ef99766e16
Anu Priya <39757552+ladybug-anu@users.noreply.github.com>
2026-08-16 14:45:37 +0530

    read log function

--- marathon/Log Parser/src/log_parser.py
+++ marathon/Log Parser/src/log_parser.py
@@ -1,5 +1,16 @@
-def read_log():
-    pass
+import os 
+
+def read_log(file):
+    try:
+        with open(file=file, mode='r') as f:
+            lines = f.read()
+        return lines
+    except FileNotFoundError:
+        return "File not found"
+    except PermissionError:
+        return "don't have permission to access file"
+    except Exception as e:
+        print(f"Something else broke: {e}")
 
 def parse_line():
     pass
@@ -16,3 +27,8 @@ def common_messages():
 def save_report_to_json():
     pass
 
+if __name__ == "__main__":
+    print(os.getcwd())
+    lines = read_log("../sample.log")
+  
+  
